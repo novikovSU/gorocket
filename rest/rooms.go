b1b230e739f84c6e376c788f7eadf5d49d1d8945
Sergey Novikov <novikov@Sergeys-MacBook-Air.local>
2019-06-24 17:52:35 +0500

    Add file upload function

--- rest/rooms.go
+++ rest/rooms.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -22,6 +24,35 @@ type RoomResponse struct {
 	Update []api.Room `json:"update"`
 	Remove []api.Room `json:"remove"`
 	SuccessResponse
+}
+
+// RoomsUploadOptions AAA
+type RoomsUploadOptions struct {
+	RoomID      string `json:"rid"`
+	File        string `json:"file"`
+	Messager    string `json:"msg,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+// RoomsUploadResponse AAA
+type RoomsUploadResponse struct {
+	SuccessResponse
+}
+
+// Upload sends a message with a file to a room. The name of the room has to be not nil.
+//
+// https://rocket.chat/docs/developer-guides/rest-api/rooms/upload/
+func (r *Rooms) Upload(opts *RoomsUploadOptions) (*RoomsUploadResponse, error) {
+	data, err := json.Marshal(&opts)
+	if err != nil {
+		return nil, err
+	}
+	request, _ := http.NewRequest("POST", r.client.getURL()+"/api/v1/rooms.upload/"+opts.RoomID, bytes.NewBuffer(data))
+
+	response := new(RoomsUploadResponse)
+	err = r.client.doRequest(request, response)
+
+	return response, err
 }
 
 // Get AAA
